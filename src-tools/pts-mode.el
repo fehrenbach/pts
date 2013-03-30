a8f753a3af138d5e51e2e78abbdce0dac0453a5b
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2013-03-30 13:52:24 +0100

    Programmatic fontification experiments

--- src-tools/pts-mode.el
+++ src-tools/pts-mode.el
@@ -1,3 +1,43 @@
+;;; Programmatic font locking
+
+(require 'cl)
+
+(defmacro comment (&rest body) nil)
+
+;; example stmt:
+(comment assert (lambda (x) (inc x))(5) = 5 + 1 : inc function does stupid things)
+;; and it's colouring
+(defvar example nil)
+(setq example
+      '(((110 . 116) assert)
+        ((118 . 124) lambda)
+        ((126 . 127) (var-dec x1))
+        ((135 . 138) (var-ref inc2))
+        ((139 . 140) (var-ref x1))
+        ((149 . 182) doc) ;; 149 is the colon
+;;        ((151 . 182) . string)
+        ))
+
+(defun select-face (kind)
+  (pcase (first kind)
+    (`assert font-lock-builtin-face)
+    (`string font-lock-string-face)
+    (`doc font-lock-doc-face)
+    (`(var-dec ,ref) font-lock-variable-name-face)))
+
+(defun fontify-accordingly (list)
+  (loop for ((min . max) . kind) in list
+        for face = (select-face kind)
+        initially (remove-text-properties (point-min) (point-max) '(font-lock-face))
+        when face
+        do (put-text-property min max 'font-lock-face face)))
+
+(fontify-accordingly example)
+
+;;(run-with-idle-timer 1 :repeat (lambda () (fontify-accordingly example)))
+
+;;; PTS mode
+
 (defvar pts-mode-hook nil)
 
 (defvar pts-mode-map
@@ -6,31 +46,14 @@
     map)
   "Keymap for PTS major mode")
 
-
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.lpts\\'" . pts-mode))
-
-(defconst pts-mode-font-lock-keywords
-  `(
-    (,(regexp-opt '("lambda" "Pi" "if0" "then" "else" "assert" "module")) . font-lock-builtin-face)
-    (,(regexp-opt '("Int")) . font-lock-type-face)))
-
-(defvar pts-mode-syntax-table
-  (let ((st (make-syntax-table)))
-    (modify-syntax-entry ?\; ".1")
-    (modify-syntax-entry ?\n ".2")
-    (modify-syntax-entry ?> ".3" st)
-    (modify-syntax-entry ?\  ".4b" st)
-    st)
-  "Syntax table for PTS major mode")
 
 (defun pts-mode ()
   "Major mode for editing PTS files"
   (interactive)
   (kill-all-local-variables)
-  (set-syntax-table pts-mode-syntax-table)
   (use-local-map pts-mode-map)
-  (set (make-local-variable 'font-lock-defaults) '(pts-mode-font-lock-keywords))
   (setq major-mode 'pts-mode)
   (setq mode-name "PTS")
   (run-hooks 'pts-mode-hook))
